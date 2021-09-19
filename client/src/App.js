b4e92c293d8bdc7560ffa706106f2f04a322e5bd
tanishkul <tanishkul@gmail.com>
2021-09-19 22:09:37 +0530

    Added Create and Update Profile

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Alert from './components/layouts/Alert';
 import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-form/CreateProfile';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -35,6 +36,11 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
